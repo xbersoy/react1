71a47181a121d08e41915795d55ab51a4e181ccc
burak ersoy <burakersoy@msn.com>
2019-10-04 17:08:43 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit and save to reload.
+            <br></br>
+            <br></br>
+            test
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Website
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+            <br></br>
+            But you won't be able to how to learn this shit still.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
